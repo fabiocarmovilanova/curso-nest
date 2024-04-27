e9af29df598ead78f976b5c3f0f6872ffb5f6d26
fabiocarmovilanova <fabio.carmo@vilanova.com.br>
2024-04-27 15:30:38 -0300

    Create first method post

--- src/users/user.controllers.ts
+++ src/users/user.controllers.ts
@@ -1,13 +1,37 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { randomUUID } from 'crypto';
 
 type ParamUser = {
   id: string
 }
 
-@Controller()
+type QueryUser = {
+  p: string
+  r: string
+}
+
+type BodyUser = {
+  name: string,
+  age: number
+}
+
+@Controller("/users")
 export class UserController {
-  @Get('/users/:id')
+  @Get('/find/:id')
   findByid(@Param() params: ParamUser) {
     return 'usuario é o: ' + params.id;
+  }
+
+  @Get('/findbypages')
+  findByPages(@Query() queries:any){
+    return "Queries " + JSON.stringify(queries)
+  }
+
+  @Post('/create')
+  create(@Body() data:BodyUser){
+    return {
+      ...data,
+      id: randomUUID()
+    }
   }
 }
